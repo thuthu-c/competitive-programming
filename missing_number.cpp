48e7a1bf5c485b2cb484c99adaad4501bc95690c
thuthu-c <thuannyba@gmail.com>
2024-01-08 22:02:19 -0300

    add cses problems

---
+++ missing_number.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+    unsigned long long int n; 
+    vector<unsigned long long int> numbers; 
+
+    for(unsigned long long int i; i < n-1; ++i){
+        cin>>numbers[i];
+    }
+
+    return 0;
+}
