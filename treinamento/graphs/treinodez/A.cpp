49d142737a945c4044a10d9d6486f361b688a508
thuthu-c <thuannyba@gmail.com>
2024-07-23 22:16:55 -0300

    feat: add correct answer about route message CSES

--- treinamento/graphs/treinodez/A.cpp
+++ treinamento/graphs/treinodez/A.cpp
@@ -2,86 +2,64 @@
 using namespace std;
 #define ll long long int
 
-
-
-int main(){
-
+int main() {
     ll n, m, temp_A, temp_B; 
 
     cin >> n >> m; 
 
-    set<ll> graph[n+1];
+    vector<ll> graph[n+1];  // Using vector instead of set for adjacency list
     queue<ll> ativa; 
-    bool visited[n+1]; 
+    vector<bool> visited(n+1, false);  // Initialize visited with false
     vector<ll> distancia(n+1, 0);
-    ll p[n];
-    
+    vector<ll> p(n+1, -1);  // Initialize parent array with size n+1 and default value -1
 
-    for (auto i{0}; i < m; ++i){
-        cin>>temp_A >> temp_B;
-
-        graph[temp_A].insert(temp_B);
-        graph[temp_B].insert(temp_A);
-        cout<< "O B.O NÃO É AQUI"<< endl; 
+    for (auto i = 0; i < m; ++i) {
+        cin >> temp_A >> temp_B;
+        graph[temp_A].push_back(temp_B);
+        graph[temp_B].push_back(temp_A);
     }
 
-    cout << "saiu do for???"<< endl; 
     ativa.push(1);
     visited[1] = true;
-    cout<<"visted é: " << visited[1] << endl; 
 
-   
-
-    while(!ativa.empty()){
+    while (!ativa.empty()) {
         ll local = ativa.front();
-        cout << "LOCAL É: " << local << endl;  
         ativa.pop();
 
-        
-        for (auto vi : visited){
-        cout << "visited: " << vi << endl;
-        } 
-
-        for(auto vertice : graph[local]){
-            cout << "visited[vertice]: " << visited[vertice] << endl; 
-            cout << "vertice: "<< vertice << endl;
-
-            if(!visited[vertice]){
+        for (auto vertice : graph[local]) {
+            if (!visited[vertice]) {
                 distancia[vertice] = distancia[local] + 1;
                 visited[vertice] = true;
-                p[vertice]  = local;
+                p[vertice] = local;
                 ativa.push(vertice);
             }
         }
     }
 
-    if(!visited[n]){
-        cout<<"IMPOSSIBLE" << endl;
+    if (!visited[n]) {
+        cout << "IMPOSSIBLE" << endl;
         return 0;
-    } 
-
-    cout<<"o problema sou eu" << endl;
+    }
 
     ll local = n; 
     ll k = distancia[n];
-    cout<< distancia[n-1] << endl; 
-    vector<ll> resposta(k+1);
+    vector<ll> resposta;
 
-    for(auto i{k}; i >= 0; --i){
-        cout<<"o problema sou eu" << endl;
-        resposta[i] = local;
+    while (local != -1) {  // Construct path in reverse using parent array
+        resposta.push_back(local);
         local = p[local];
     }
 
-    cout<< k + 1 << endl;
+    reverse(resposta.begin(), resposta.end());  // Reverse the path to get the correct order
 
-    for (auto i{0}; i <=k; ++i){
-        if(i != k){
-            cout << resposta[i] << " ";
-        }else cout << resposta[i] << endl; 
-        
+    cout << k + 1 << endl;
+    for (size_t i = 0; i < resposta.size(); ++i) {
+        cout << resposta[i];
+        if (i != resposta.size() - 1) {
+            cout << " ";
+        }
     }
-
+    cout << endl;
 
     return 0; 
 }
